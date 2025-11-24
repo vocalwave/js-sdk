d368dbfa2ea0cc2682480e073bf2da1e8443309b
vocalwave <pauld@adxnetwork.com>
2025-11-23 22:45:19 -0800

    Add src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * QRNG API - JavaScript/TypeScript SDK
+ * Official client library for the QRNG API
+ */
+
+export { QRNGClient } from './client';
+export { QRNGStreamClient } from './stream';
+export * from './types';
+export * from './errors';
